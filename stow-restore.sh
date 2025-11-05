a544430fa4cdb4e3ba177f2033d6e26ba68d8917
Nathan Hoyt <slimnatekc@gmail.com>
2025-11-05 13:30:06 -0600

    Some fixes to restore script

--- stow-restore.sh
+++ stow-restore.sh
@@ -5,12 +5,16 @@
 
 DOTFILES_DIR=~/.dotfiles
 CONFIG_DIR=~/.config
+BACKUP_DIR=$CONFIG_DIR/.config_backup
+
+# Create the backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
 
 # Backup the existing configuration files
-mv $CONFIG_DIR/hypr $CONFIG_DIR/_config_backup/hypr.backup
-mv $CONFIG_DIR/waybar $CONFIG_DIR/_config_backup/waybar.backup
-mv $CONFIG_DIR/bash $CONFIG_DIR/_config_backup/bash.backup
-mv $HOME/.bashrc $HOME/._config_backup/bashrc.backup
+mv $CONFIG_DIR/hypr $BACKUP_DIR/hypr.backup
+mv $CONFIG_DIR/waybar $BACKUP_DIR/waybar.backup
+mv $CONFIG_DIR/bash $BACKUP_DIR/bash.backup
+mv $HOME/.bashrc $BACKUP_DIR/bashrc.backup
 
 # Create the configuration file directories if they don't exist
 mkdir -p $CONFIG_DIR/hypr
@@ -21,7 +25,7 @@ mkdir -p $CONFIG_DIR/bash
 stow --D -v -d $DOTFILES_DIR -t $CONFIG_DIR/hypr hypr
 stow --D -v -d $DOTFILES_DIR -t $CONFIG_DIR/waybar waybar
 stow --D -v -d $DOTFILES_DIR -t $CONFIG_DIR/bash bash
-stow --D -v -n -d $DOTFILES_DIR -t ~ bashrc
+stow --D -v -d $DOTFILES_DIR -t ~ bashrc
 
 # Restore the configuration files from the dotfiles repository
 stow -v -d $DOTFILES_DIR -t $CONFIG_DIR/hypr hypr
@@ -30,5 +34,6 @@ stow -v -d $DOTFILES_DIR -t $CONFIG_DIR/bash bash
 stow --dotfiles -v -d $DOTFILES_DIR -t ~ bashrc
 
 # Install microsoft-edge-stable-bin from AUR and configure as default browser
-sudo pacman -S microsoft-edge-stable-bin
-xdg-settings set default-web-browser microsoft-edge-stable-bin.desktop
+# Commented out becuase pacman does not have edge. Prob have to use yay instead.
+# sudo pacman -S microsoft-edge-stable-bin
+# xdg-settings set default-web-browser microsoft-edge-stable-bin.desktop
